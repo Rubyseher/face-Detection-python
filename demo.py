33115296eff61a50501c2329d47896e9da976a1e
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2022-08-17 21:17:06 +0530

    video face detection + close button

--- demo.py
+++ demo.py
@@ -2,15 +2,26 @@ import cv2
 from random import randrange 
 trained_face_data =cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img= cv2.imread('people.jpg')
-greyscaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+webcam= cv2.VideoCapture(0)
+while True:
+        successfull_frame_read,frame=webcam.read()
 
-face_coordinates=trained_face_data.detectMultiScale(greyscaled_img)
-print(face_coordinates)
+        # img= cv2.imread('people.jpg')
+        greyscaled_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        face_coordinates=trained_face_data.detectMultiScale(greyscaled_img)
 
-for x,y,w,h in face_coordinates:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(randrange(128,256),randrange(128,256),randrange(128,256)),10)
-cv2.imshow("i am iron man",img)
+        for x,y,w,h in face_coordinates:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(randrange(128,256),randrange(128,256),randrange(128,256)),10)
 
-cv2.waitKey()
-print("hi")
+        cv2.imshow("i am iron man",frame)
+
+        key=cv2.waitKey(1)
+        if key==81 or key==113:
+            break
+webcam.release()
+# print(face_coordinates)
+
+# # cv2.imshow("i am iron man",img)
+
+# cv2.waitKey()
+# print("hi")
